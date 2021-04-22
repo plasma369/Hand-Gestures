789f25059151dda6eb5c995260092cec11cf098b
plasma369 <76848965+plasma369@users.noreply.github.com>
2021-04-22 16:48:31 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -19,25 +19,66 @@ function take_snapshot()
 
   console.log('ml5 version:', ml5.version);
   
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/IzXvkf1K_/',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/IzXvkf1K_/model.json',modelLoaded);
 
-  function modelLoaded() {
+function modelLoaded()
+{
     console.log('Model Loaded!');
-  }
-      
-  function check()
-  {
+}
+
+function check()
+{
     img = document.getElementById('captured_image');
     classifier.classify(img, gotResult);
-  }
+}
 
+function speak()
+{
+    var synth = window.speachSynthesis;
+    speak_data_1 = "The first pridiction is " + pridiction_1
+    speak_data_2 = "And the second pridiction is " + pridiction_2;
+    var utterThis = new SpeechSynthisisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
 
-function gotResult(error, results) {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log(results);
-    document.getElementById("result_object_name").innerHTML = results[0].label;
-    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
-  }
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(result);
+        document.getElementById("result_hand_name").innerHTML = results[0].label;
+        document.getElementById("result_hand_name2").innerHTML = results[1].label;
+        prediction_1 = result[0].label;
+        prediction_2 = result[1].label;
+        speak()
+        if(result[0].label == "👌")
+        {
+            document.getElementById("updated_emoji").innerHTML = "👌"
+        }
+        if(result[0].label == "👍")
+        {
+            document.getElementById("updated_emoji").innerHTML = "👍"
+        }
+        if(result[0].label == "✌")
+        {
+            document.getElementById("updated_emoji").innerHTML = "✌"
+        }
+
+        if(result[0].label == "👌")
+        {
+            document.getElementById("updated_emoji2").innerHTML = "👌"
+        }
+        if(result[0].label == "👍")
+        {
+            document.getElementById("updated_emoj2i").innerHTML = "👍"
+        }
+        if(result[0].label == "✌")
+        {
+            document.getElementById("updated_emoji2").innerHTML = "✌"
+        }
+    }
 }
